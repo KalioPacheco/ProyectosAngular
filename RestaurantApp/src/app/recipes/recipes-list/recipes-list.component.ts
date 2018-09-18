9b9cbf9e282ded75eeede96f90f6bf631e6c124f
KalioPacheco <crack11110@hotmail.com>
2018-09-18 17:54:22 -0500

    Versión del 18 de septiembre

--- RestaurantApp/src/app/recipes/recipes-list/recipes-list.component.ts
+++ RestaurantApp/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,6 +7,8 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+
+  @Output() recipeItem = new EventEmitter<Recipe>();
 
   recipes: Recipe[]=[
     new Recipe('Ejemplo 1', 'this a example','https://www.stockthehouse.com/wp-content/uploads/2018/06/pep-pizza.jpg'),
