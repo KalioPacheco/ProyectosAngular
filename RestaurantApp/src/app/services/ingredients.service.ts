4f46fc3242e21d80898090bfc767aecdae4b68b1
KalioPacheco <crack11110@hotmail.com>
2018-10-17 22:29:18 -0500

    2° Actualización 17 de Octubre

--- RestaurantApp/src/app/services/ingredients.service.ts
+++ RestaurantApp/src/app/services/ingredients.service.ts
@@ -3,26 +3,27 @@ import { EventEmitter } from "@angular/core";
 
 export class IngredientsService {
     ingredientsChanged = new EventEmitter<Ingredient[]>();
-    private ingredients: Ingredient [] = [
+    private ingredients: Ingredient[] = [
         new Ingredient('Tomatoes', 5),
         new Ingredient('Apples', 3)
-      ];
+    ];
 
-      getIngredients(){
-          return this.ingredients.slice();
-      }
+    getIngredients() {
+        return this.ingredients.slice();
+    }
 
-      addIngredient(ingredient: Ingredient){
-          this.ingredients.push(ingredient);
-          this.ingredientsChanged.emit(this.ingredients.slice());
-      }
+    addIngredient(ingredient: Ingredient) {
+        this.ingredients.push(ingredient);
+        this.ingredientsChanged.emit(this.ingredients.slice());
+    }
 
-      //Una vez obtenidos se deben guardar en el arreglo, con un foreach donde se obtiene el objeto con una constante i
-      addIngredients(auxIngredients: Ingredient[]){
-        for(const i of auxIngredients){
-            this.ingredients.push(i);
+    //Una vez obtenidos se deben guardar en el arreglo, con un foreach donde se obtiene el objeto con una constante i
+    addIngredients(auxIngredients: Ingredient[]) {
+        for (const i of auxIngredients) {
+            let array = this.ingredients.find(element => element.name==i.name);
+            (array)?array.amount=array.amount+i.amount:this.ingredients.push(i)
         }
         //Una vez esto se debera volvera a ejecutar el evento con el emitter
         this.ingredientsChanged.emit(this.ingredients.slice());
-      }
+    }
 }
