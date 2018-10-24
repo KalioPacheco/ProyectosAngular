4f6e602cc1d80e4ee09ed0f800b3e23cf52ca9ce
KalioPacheco <crack11110@hotmail.com>
2018-10-23 23:42:49 -0500

    Actualizacion

--- RestaurantApp/src/app/services/ingredients.service.ts
+++ RestaurantApp/src/app/services/ingredients.service.ts
@@ -20,8 +20,7 @@ export class IngredientsService {
     //Una vez obtenidos se deben guardar en el arreglo, con un foreach donde se obtiene el objeto con una constante i
     addIngredients(auxIngredients: Ingredient[]) {
         for (const i of auxIngredients) {
-            let array = this.ingredients.find(element => element.name==i.name);
-            (array)?array.amount=array.amount+i.amount:this.ingredients.push(i)
+            (this.ingredients.find(element => element.name==i.name))?this.ingredients.find(element => element.name==i.name).amount=this.ingredients.find(element => element.name==i.name).amount+i.amount:this.ingredients.push(i)
         }
         //Una vez esto se debera volvera a ejecutar el evento con el emitter
         this.ingredientsChanged.emit(this.ingredients.slice());
--- RestaurantApp/src/app/services/recipe.service.ts
+++ RestaurantApp/src/app/services/recipe.service.ts
@@ -6,34 +6,38 @@ import { IngredientsService } from "./ingredients.service";
 
 @Injectable() // Decorador para poder agregar un servicio dentro de otro
 export class RecipeService {
-    recipeSelected = new EventEmitter<Recipe>();
-    
+    //recipeSelected = new EventEmitter<Recipe>();
+
     private recipes: Recipe[] = [
-        new Recipe('A test recipe 1', 'This is a simply test ', 'http://sevilla.abc.es/contenidopromocionado/wp-content/uploads/sites/2/2017/09/1996x1206-hamburguesas.jpg', 
-    [
-        new Ingredient('bread', 2),
-        new Ingredient('tomatoes', 3)
-    ]),
+        new Recipe('A test recipe 1', 'This is a simply test ', 'http://sevilla.abc.es/contenidopromocionado/wp-content/uploads/sites/2/2017/09/1996x1206-hamburguesas.jpg',
+            [
+                new Ingredient('bread', 2),
+                new Ingredient('tomatoes', 3)
+            ]),
         new Recipe('A test recipe 2', 'This is a simply test', 'http://sevilla.abc.es/contenidopromocionado/wp-content/uploads/sites/2/2017/09/1996x1206-hamburguesas.jpg',
-        [
-            new Ingredient('Beef', 3),
-            new Ingredient('garlic', 1)
-        ]),
+            [
+                new Ingredient('Beef', 3),
+                new Ingredient('garlic', 1)
+            ]),
         new Recipe('A test recipe 3', 'This is a simply test', 'http://sevilla.abc.es/contenidopromocionado/wp-content/uploads/sites/2/2017/09/1996x1206-hamburguesas.jpg',
-        [
-            new Ingredient('Sauce', 2),
-        ])
+            [
+                new Ingredient('Sauce', 2),
+            ])
     ];
 
     //Crear constructor para utilizar el ingredientsService
-    constructor(private ingredientsService: IngredientsService){}
+    constructor(private ingredientsService: IngredientsService) { }
 
-      getRecipes(){
-          return this.recipes.slice();
-      }
+    getRecipes() {
+        return this.recipes.slice();
+    }
 
-      //Metodo para invocar desde la vista
-      addIngredientsToShoppingList(ingredients: Ingredient[]){
+    getRecipe( index: number) {
+        return this.recipes[index];
+    }
+
+    //Metodo para invocar desde la vista
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
         this.ingredientsService.addIngredients(ingredients);
-      }
+    }
 }
