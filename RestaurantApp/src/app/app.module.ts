b09ad41f2d4d7d3724cd010e3958e418b1429cb1
KalioPacheco <crack11110@homail.com>
2018-10-30 17:36:38 -0600

    Actualización 30 de Octubre

--- RestaurantApp/src/app/app.module.ts
+++ RestaurantApp/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -30,7 +31,8 @@ import { RecipesEditComponent } from './recipes/recipes-edit/recipes-edit.compon
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [IngredientsService],
   bootstrap: [AppComponent]
--- RestaurantApp/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ RestaurantApp/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,27 +1,52 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, OnDestroy } from '@angular/core';
 import { IngredientsService } from '../../services/ingredients.service';
 import { Ingredient } from '../../shared/ingredient.model';
+import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css']
+  styleUrls: ['./shopping-edit.component.css'],
+  
 })
-export class ShoppingEditComponent implements OnInit {
+export class ShoppingEditComponent implements OnInit, OnDestroy {
 
-  @ViewChild('nameInput') nameInputRef: ElementRef;
-  @ViewChild('amountInput') amountInputRef: ElementRef;
-
+  //@ViewChild('nameInput') nameInputRef: ElementRef;
+  //@ViewChild('amountInput') amountInputRef: ElementRef;
+  private subscription: Subscription;
+  private editedItem: Ingredient;
+  @ViewChild('f')slForm: NgForm;
+  editMode = false;
+  indexEditedItem: number;
   constructor(private ingredientsService: IngredientsService) { }
 
   ngOnInit() {
+    this.subscription = this.ingredientsService.startedEditing.subscribe(
+      (index: number)=>{
+        this.indexEditedItem = index;
+        this.editedItem = this.ingredientsService.getIngredient(index);
+        this.slForm.setValue({name: this.editedItem.name, amaunt: this.editedItem.amount})
+        this.editMode = true;
+      }
+    );
   }
 
-  onAddItem(){
-    const name = this.nameInputRef.nativeElement.value;
-    const amount = this.amountInputRef.nativeElement.value;
-    
-    const newIngredient = new Ingredient(name, amount);
-    this.ingredientsService.addIngredient(newIngredient);
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
   }
+
+  onAddItem(form: NgForm){
+    //const name = this.nameInputRef.nativeElement.value;
+    //const amount = this.amountInputRef.nativeElement.value;
+    if(this.editMode){
+      this.ingredientsService.updateIngredient(this.indexEditedItem, new Ingredient(form.value.name, form.value.amaunt));
+    } else {
+      this.ingredientsService.addIngredient(new Ingredient(form.value.name, form.value.amaunt));
+    }
+    
+  }
+
+
+
 } 
